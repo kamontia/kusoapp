b5d7f5ebeb3919846d9dc64efb3ca5f9c4be2fab
Tatsuya Kamohara <17017563+kamontia@users.noreply.github.com>
2018-12-11 16:13:43 +0900

    Hello world via Echo framework

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"./handler"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	// インスタンスの作成
+	e := echo.New()
+
+	// Echoのミドルウェアを使用する場合は以下のように追記
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// ルーティング
+	e.GET("/hello", handler.Hello())
+
+	// サーバー起動
+	e.Start(":80") //ポート番号指定してね
+}
